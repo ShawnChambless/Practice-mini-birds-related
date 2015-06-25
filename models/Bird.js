888c93c8fa4cf6f312e4b470191443f52790eaaf
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-25 14:38:50 -0600

    add bird schema

---
+++ models/Bird.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var birdSchema = new mongoose.Schema({
+    name: { type: String, required: true, lowercase: true },
+    order: { type: String, maxlength: 20, required: true, lowercase: true },
+    status: { type: String, lowercase: true, enum:[
+        'extinct',
+        'extinct in the wild',
+        'critically endangered',
+        'endangered',
+        'vulnerable',
+        'near threatened',
+        'conservation dependent',
+        'least concern'
+        ]
+    }
+});
+
+
+module.exports = mongoose.model('Bird', birdSchema);
