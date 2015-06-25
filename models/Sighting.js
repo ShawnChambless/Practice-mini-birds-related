a9a6efc217ae5cfb72d99ef3d3dc4ae72dc824b2
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-25 15:31:34 -0600

    fixed imports for real, added user schema require

--- models/Sighting.js
+++ models/Sighting.js
@@ -1,24 +1,12 @@
 var mongoose = require('mongoose');
-var birdSchema = require('bird')
+var birdSchema = require('./Bird');
+var userSchema = require('./User');
+
 var sightingSchema = new mongoose.Schema({
-  name: { type: String, lowercase: true },
-  order: { type: String, lowercase: true, maxlength: 20 },
-  status: {
-    type: String,
-    lowercase: true,
-    enum: [
-      'extinct',
-      'extinct in the wild',
-      'critically endangered',
-      'endangered',
-      'vulnerable',
-      'near threatened',
-      'conservation dependent',
-      'least concern'
-    ]
-  },
-  confirmed: { type: Boolean, default: false },
-  numberSeen: { type: Number, min: 1 }
+    user: [userSchema],
+    bird: [birdSchema],
+    confirmed: { type: Boolean, default: false },
+    numberSeen: { type: Number, min: 1 }
 });
 
 
